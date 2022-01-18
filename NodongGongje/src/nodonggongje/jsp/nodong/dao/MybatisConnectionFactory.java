831aa5e0a9548730fb367fdb729dd88fc0354297
SonYoungDeuk1 <deukzzang@gmail.com>
2022-01-18 10:00:51 +0900

    dao upload

---
+++ NodongGongje/src/nodonggongje/jsp/nodong/dao/MybatisConnectionFactory.java
@@ -0,0 +1,37 @@
+package nodonggongje.jsp.nodong.dao;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MybatisConnectionFactory {
+	
+	private static SqlSessionFactory sqlSessionFactory;
+	
+	static{
+		try {
+			Reader reader = Resources.getResourceAsReader("nodonggongje/jsp/nodong/dao/config.xml");
+			
+			if(sqlSessionFactory == null){
+				sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+			}
+		}catch (FileNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static SqlSession getSqlSession(){
+		return sqlSessionFactory.openSession();
+	}
+
+}
